b05bb426c91b4da97b0d7c603a946e34f058832d
Jeff <Xavierjeff451@gmail.com>
2022-04-30 21:58:12 +0300

    commit

--- app/models/news.py
+++ app/models/news.py
@@ -3,5 +3,6 @@ class News_source:
     News class to define New Objects
     '''
 
-    def __init__(self,name):
+    def __init__(self,id,name):
+        self.id = id
         self.name = name
--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
 NEWS_API_KEY = "4fd2b95bee8d40c581f5588f3a0a9c35"
+NEWS_BASE_URL = "https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
